1eeb9f670fea5baf2e886f96c121789aa71bd03b
Lordrakha <95117109+Lordrakha@users.noreply.github.com>
2021-11-29 15:15:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,28 +2,28 @@ global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
-  '62857139649630': { // put your number here
+  '6285722187094': { // put your number here
     name: 'Owner Metro Bot/Creator games-wabot',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: false, // if true this number will send if user use command /report or /join
     isCreator: true // if true this number will send if user use command /owner or /creator
   },
-  '6281390658325': { // put your number here
+  '625722187094': { // put your number here
     isDev: true, // if true this number will send if bot have bug and developer mode true
     isReport: true, // if true this number will send if user use command /report or /join
     isCreator: false // if true this number will send if user use command /owner or /creator
   },
-  '6282256115584': { // put your number here
+  '6285722187094': { // put your number here
     name: 'Dave',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: true, // if true this number will send if user use command /report or /join
     isCreator: false // if true this number will send if user use command /owner or /creator
   },
-  '60199782326': {
+  '6285722187094': {
     name: 'Adiixyz',
     isReport: true, // if true this number will send if user use command /report or /join
   },
-  '6285713964963': {
+  '6285722187094': {
     isReport: true
   },
   '13479805233': {} // ignore if you don't want anything, like name dll
